96a216a6abfba2b3e773327c2e491b5c772c7293
Lumine05 <99068024+Lumine05@users.noreply.github.com>
2024-07-09 16:13:10 +0800

    initial commit
    just fixed and moved

--- Scenes/RootScripts/WorldScript.gd
+++ Scenes/RootScripts/WorldScript.gd
@@ -4,7 +4,7 @@ var parameters: Dictionary
 var Duration = 5
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var LevelTitle = get_node("PlayerNode/TitleAnim")
+	var LevelTitle = get_node("PlayerNode/Player/CanvasLayer/TitleAnim")
 	LevelTitle.play("Level1")
 	start_cooldown_timer()
 	pass # Replace with function body.
@@ -19,13 +19,13 @@ func start_cooldown_timer():
 	# Start a timer to reset the teleporting state after the cooldown period
 	$MusicTimer.start(Duration)
 func _on_music_timer_timeout():
-	var StartHov = get_node("PlayerNode/Music/AnimationPlayer")
+	var StartHov = get_node("PlayerNode/Player/CanvasLayer/Music/AnimationPlayer")
 	StartHov.play("Music")
 	$MusicTimer.stop()
 	pass # Replace with function body.
 
 
 func _on_animation_player_animation_finished(Music):
-	var StartHov = get_node("PlayerNode/Music")
+	var StartHov = get_node("PlayerNode/Player/CanvasLayer/Music")
 	StartHov.hide()
 	pass # Replace with function body.
