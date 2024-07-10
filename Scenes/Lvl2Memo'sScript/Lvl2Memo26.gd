a38e7b09e4fa610cf66ef76daab11681bcf5157b
Qtkfrfr <99068024+Qtkfrfr082@users.noreply.github.com>
2024-07-10 21:00:00 +0800

    initial commit
    Added Dialogue Script for Lvl2

---
+++ Scenes/Lvl2Memo'sScript/Lvl2Memo26.gd
@@ -0,0 +1,32 @@
+extends Area2D
+const Balloon = preload("res://addons/dialogue_manager/example_balloon/example_balloon.tscn")
+var player_inside
+#This is dialouge script
+
+@export var dialogue_resource: DialogueResource
+@export var dialogue_start: String = "Memo26"
+func _process(_delta):
+	if player_inside and Input.is_action_just_pressed("interact"):
+			MemoShow()
+	pass
+	
+func MemoShow():
+	var balloon: Node = Balloon.instantiate()
+	get_tree().current_scene.add_child(balloon)
+	balloon.start(dialogue_resource, dialogue_start)
+	
+func _on_body_entered(body):
+	if body.name == "Player":
+		$Interact.show()
+		$Interact.play("interact")
+		player_inside = true
+		
+
+	pass # Replace with function body.
+
+
+func _on_body_exited(body):
+	if body.name == "Player":
+		$Interact.hide()
+		player_inside = false
+	pass # Replace with function body.
